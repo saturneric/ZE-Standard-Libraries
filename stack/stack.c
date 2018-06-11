785a6236faafb2c0a4c1f6e47da355c3a934a16b
Saturneic <huyibing@huyibingdeAir>
2018-06-11 16:00:48 +0800

    测试

--- stack/stack.c
+++ stack/stack.c
@@ -9,6 +9,13 @@
 #include "stack_expand.h"
 
 int main(int argc, char **argv){
-    
+    Stack *t_stack = initStack();
+    for(int i = 0; i < 10; i++){
+        pushStack(t_stack, snodeWithInt(i));
+    }
+    for(int i = 0; i < 10; i++){
+        printf("%d",getValueByIntForSNode(popStack(t_stack)));
+    }
+    releaseStack(t_stack);
     return 0;
 }
